0e0a90bb4b42c96e7052c8570197f7c9c64dc90e
demo <onidemorng@gmail.com>
2026-09-05 04:26:28 +0900

    poker

--- Game/logic.cpp
+++ Game/logic.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 
-vector<vector<pair<string,int>>> cards(pair<string,int>& card){
+vector<vector<pair<string,int>>> cards(){
     // Black Spade
     vector<pair<string, int>> spades;
     for (int i = 1; i<=13; i++){
@@ -27,8 +28,30 @@ vector<vector<pair<string,int>>> cards(pair<string,int>& card){
     for (int i=1; i<=13; i++){
         hearts.push_back(make_pair("Heart", i));
     }
+
+    return {spades, clubs, diamonds, hearts};
+}
+
+vector<pair<string, int>> players(){
+    int n;
+    vector<pair<string, int>> player_id;
+
+    cout<<"how many players do you want to play against ?(10 is max): "<<endl;
+    while (true){
+        cin>> n;
+
+        if(n >=2 && n<=10)
+            break;
+        
+        cout<<"Enter the Correct number of players to play against "<<endl;
+    }
+    for(int i = 2; i<=n; i++){
+            player_id.push_back(make_pair(" Bot ", i));
+    }
+
+    return player_id;
 }
 
 void logic(){
-    
+
 }
